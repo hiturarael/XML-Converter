4a7480fdff0b1c4f7b84ebcea6fcc5d89894e694
hiturarael <hiturarael@hotmail.com>
2015-07-18 00:47:34 -0400

    7/17/2015

--- XMLConverter/Form1.Designer.vb
+++ XMLConverter/Form1.Designer.vb
@@ -22,9 +22,46 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GetFilePath = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(28, 22)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(444, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'GetFilePath
+        '
+        Me.GetFilePath.Location = New System.Drawing.Point(478, 22)
+        Me.GetFilePath.Name = "GetFilePath"
+        Me.GetFilePath.Size = New System.Drawing.Size(95, 23)
+        Me.GetFilePath.TabIndex = 1
+        Me.GetFilePath.Text = "Open File Path"
+        Me.GetFilePath.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(616, 149)
+        Me.Controls.Add(Me.GetFilePath)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents GetFilePath As System.Windows.Forms.Button
 
 End Class
--- XMLConverter/XML.vb
+++ XMLConverter/XML.vb
@@ -1,3 +1,26 @@
-﻿Module XML
+﻿Option Strict On
+Imports Excel = Microsoft.Office.Interop.Excel
 
-End Module
+Public Class XML
+
+    Public APP As New Excel.Application
+    Public Workbook As Excel.Workbook
+    Public worksheet As Excel.Worksheet
+
+    Public worksheetNames() As String
+    Sub startRecord()
+        getSheetNames()
+
+    End Sub
+
+    Sub getSheetNames()
+
+        Workbook = APP.ActiveWorkbook
+
+        For i = 1 To Workbook.Sheets.Count
+            worksheetNames(i) = Workbook.Name.ToString
+        Next
+
+    End Sub
+
+End Class
