4f16e78605e7ffda61fa587045aed3ae64f8f489
hiturarael <hiturarael@hotmail.com>
2015-07-19 18:56:37 -0400

    get file paths

--- XMLConverter/Form1.Designer.vb
+++ XMLConverter/Form1.Designer.vb
@@ -22,21 +22,20 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.filePathTbox = New System.Windows.Forms.TextBox()
         Me.GetFilePath = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.convertPathTBox = New System.Windows.Forms.TextBox()
+        Me.ConvertPathBtn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'OpenFileDialog1
+        'filePathTbox
         '
-        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(28, 22)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(444, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.filePathTbox.Location = New System.Drawing.Point(28, 22)
+        Me.filePathTbox.Name = "filePathTbox"
+        Me.filePathTbox.Size = New System.Drawing.Size(444, 20)
+        Me.filePathTbox.TabIndex = 0
         '
         'GetFilePath
         '
@@ -47,21 +46,46 @@ Partial Class Form1
         Me.GetFilePath.Text = "Open File Path"
         Me.GetFilePath.UseVisualStyleBackColor = True
         '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'convertPathTBox
+        '
+        Me.convertPathTBox.Location = New System.Drawing.Point(28, 70)
+        Me.convertPathTBox.Name = "convertPathTBox"
+        Me.convertPathTBox.Size = New System.Drawing.Size(444, 20)
+        Me.convertPathTBox.TabIndex = 2
+        '
+        'ConvertPathBtn
+        '
+        Me.ConvertPathBtn.Location = New System.Drawing.Point(478, 68)
+        Me.ConvertPathBtn.Name = "ConvertPathBtn"
+        Me.ConvertPathBtn.Size = New System.Drawing.Size(95, 23)
+        Me.ConvertPathBtn.TabIndex = 3
+        Me.ConvertPathBtn.Text = "Convert File"
+        Me.ConvertPathBtn.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(616, 149)
+        Me.Controls.Add(Me.ConvertPathBtn)
+        Me.Controls.Add(Me.convertPathTBox)
         Me.Controls.Add(Me.GetFilePath)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.filePathTbox)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents filePathTbox As System.Windows.Forms.TextBox
     Friend WithEvents GetFilePath As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents convertPathTBox As System.Windows.Forms.TextBox
+    Friend WithEvents ConvertPathBtn As System.Windows.Forms.Button
 
 End Class
--- XMLConverter/Form1.vb
+++ XMLConverter/Form1.vb
@@ -1,3 +1,38 @@
 ﻿Public Class Form1
 
+    Private Sub GetFilePath_Click(sender As Object, e As EventArgs) Handles GetFilePath.Click
+        Dim directory As New OpenFileDialog
+        Dim apppath As String = ""
+
+        If apppath = "" Then
+            directory.Title = "Select file to convert"
+            directory.InitialDirectory = "C:\"
+            directory.Filter = "All Files (*.*)|*.*|All Files (*.*)|*.*"
+            directory.FilterIndex = 2
+            directory.RestoreDirectory = True
+
+            If directory.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                apppath = directory.FileName
+            End If
+            filePathTbox.Text = apppath
+        End If
+
+    End Sub
+
+
+    Private Sub ConvertPathBtn_Click(sender As Object, e As EventArgs) Handles ConvertPathBtn.Click
+        Dim directory As New FolderBrowserDialog
+        Dim apppath As String = ""
+
+        If apppath = "" Then
+            Dim dialog As New FolderBrowserDialog()
+            dialog.RootFolder = Environment.SpecialFolder.Desktop
+            dialog.SelectedPath = "C:\"
+            dialog.Description = "Select Application Configeration Files Path"
+            If dialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                apppath = dialog.SelectedPath
+            End If
+            convertPathTBox.Text = apppath
+        End If
+    End Sub
 End Class
