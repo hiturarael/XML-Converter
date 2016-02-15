c19c96017bede1ac82e4abce465935888286062a
hiturarael <hiturarael@hotmail.com>
2016-02-15 11:23:34 -0500

    Some work on it

--- XMLConverter/XMLConverter.vb
+++ XMLConverter/XMLConverter.vb
@@ -1,4 +1,5 @@
-﻿Option Strict On
+﻿Option Strict Off
+
 Imports Excel = Microsoft.Office.Interop.Excel
 
 Public Class XMLConverter
@@ -7,7 +8,7 @@ Public Class XMLConverter
     Private Workbook As Excel.Workbook
     Private worksheet As Excel.Worksheet
 
-    Private worksheetNames() As String
+    Private worksheetNames As New ArrayList
 
     Public Sub startRecord(ByVal path As String)
         Workbook = APP.Workbooks.Open(path)
@@ -16,15 +17,89 @@ Public Class XMLConverter
     End Sub
 
     Public Sub getSheetNames()
+        Dim sheet As Excel.Worksheet = Nothing
+        Dim sheetRows As Integer = getRecordCount()
+        Dim colCount As Integer = getColumnCount()
 
-        Workbook = APP.ActiveWorkbook
+        Try
+            For i = 1 To Workbook.Sheets.Count
+                Dim record As String = ""
+                ' Dim sections As section
+                Dim headers As New List(Of String)
+                Dim entry As New entry
 
-        Console.WriteLine(Workbook.Sheets.)
+                sheet = DirectCast(Workbook.Sheets(i), Excel.Worksheet)
 
-        For i = 1 To Workbook.Sheets.Count
-            worksheetNames(i) = Workbook.Name.ToString
-            Console.WriteLine(Workbook.Name.ToString)
-        Next
+                For rows = 1 To sheetRows
+                    For cells = 1 To colCount
+                        If sheet.Cells(rows, cells).ToString <> "" Then
+                            If rows = 1 Then
+                                headers.Add(CStr(sheet.Cells(rows, cells).value))
+                            Else
+                                entry.cellHeader.Add(headers(i))
+                                entry.cellContent.Add(CStr(sheet.Cells(rows, cells).value))
+                            End If
+                        End If
+                    Next
+                Next
+            Next
+
+            Console.WriteLine(worksheetNames)
+
+        Catch ex As Exception
+            Console.WriteLine(ex)
+        End Try
     End Sub
 
+    Private Function getRecordCount() As Integer
+        Dim sheet As Excel.Worksheet = Nothing
+        Dim rowCount As Integer = 0
+        Try
+            sheet = DirectCast(Workbook.Sheets(1), Excel.Worksheet)
+
+            For i = 1 To sheet.Rows.Count
+                If CStr(sheet.Cells(i, 1).value) <> "" Then
+                    rowCount += 1
+                Else
+                    Exit For
+                End If
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString, "An Error has Occured", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        Return rowCount
+    End Function
+
+    Private Function getColumnCount() As Integer
+        Dim sheet As Excel.Worksheet = Nothing
+        Dim colCount As Integer = 0
+        Try
+            sheet = DirectCast(Workbook.Sheets(1), Excel.Worksheet)
+
+            For i = 1 To sheet.Columns.Count
+                If CStr(sheet.Cells(1, i).value) <> "" Then
+                    colCount += 1
+                Else
+                    Exit For
+                End If
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString, "An Error has Occured", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        Return colCount
+    End Function
+
+
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
 End Class
---
+++ XMLConverter/XMLFile.vb
@@ -0,0 +1,17 @@
+﻿Option Strict On
+
+Imports System.Xml
+
+Public Class XMLFile
+    Public record As String = ""
+    Public sections As New List(Of section)
+End Class
+
+Public Class section
+    Public records As New List(Of entry)
+End Class
+
+Public Class entry
+    Public cellHeader As ArrayList
+    Public cellContent As ArrayList
+End Class
